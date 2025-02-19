8e51d9d5c8387eaa4385a5bad6c0be6a95479807
Himanshu Ahuja <106527952+himanshu-ntml@users.noreply.github.com>
2025-02-19 12:18:26 +0000

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const PlayerComponent = dynamic(() => import("./player/_components/GumletPlayerTokenExample"), { ssr: false });
+const PlayerComponent = dynamic(() => import("./_components/GumletPlayerTokenExample"), { ssr: false });
 
 export default function Player() {
     return <PlayerComponent />;
